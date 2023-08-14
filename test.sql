87eae00ceea4a3185fe4bada93c32e46a8692ba4
Tethachan <satoshi.okayama@databricks.com>
2023-08-14 14:01:23 +0900

    add suda value

--- test.sql
+++ test.sql
@@ -1,3 +1,3 @@
-CREATE OR REPLACE DATABASE suda_db_hackathon4lakehouse.trash;
+-- CREATE OR REPLACE DATABASE suda_db_hackathon4lakehouse.trash;
 CREATE TABLE suda_db_hackathon4lakehouse.trash.sqltest (id INT, name STRING, age INT);
 INSERT INTO suda_db_hackathon4lakehouse.trash.sqltest VALUES (1, 'udagawa', 39)
